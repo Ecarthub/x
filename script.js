d0c409d8ce977a2ace51730ef58575d066bc9a08
ssurvey-star <103487893+ssurvey-star@users.noreply.github.com>
2023-09-24 14:48:05 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function input7() {
+	document.getElementById('input7').style.background = "#efefef";
+} 
+function input2() {
+	document.getElementById('input2').style.background = "#efefef";
+} 
+function input3() {
+	document.getElementById('input3').style.background = "#efefef";
+} 
+function input() {
+	document.getElementById('input').style.background = "#efefef";
+} 
+function input4() {
+	document.getElementById('input4').style.background = "#efefef";
+} 
+function input5() {
+	document.getElementById('input5').style.background = "#efefef";
+} 
+function input6() {
+	document.getElementById('input6').style.background = "#efefef";
+} 
+function input8() {
+	document.getElementById('input8').style.background = "#efefef";
+} 
+function move() {
+	window.location = "facebook.html";
+}
